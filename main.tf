b366d755e93a9ae4a32f5f52f0b0e399df840850
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2026-06-07 04:25:38 -0700

    Install Helm + Grafana + Prometheus

--- main.tf
+++ main.tf
@@ -14,61 +14,61 @@ provider "aws" {
 }
 
 # Create VPC
-resource "aws_vpc" "ops4" {
+resource "aws_vpc" "ops5" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
-    Name = "ops4-vpc"
+    Name = "ops5-vpc"
   }
 }
 
 # Create subnet
-resource "aws_subnet" "ops4_public" {
-  vpc_id                  = aws_vpc.ops4.id
+resource "aws_subnet" "ops5_public" {
+  vpc_id                  = aws_vpc.ops5.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "ops4-public-subnet"
+    Name = "ops5-public-subnet"
   }
 }
 
 # Create IGW
-resource "aws_internet_gateway" "ops4_igw" {
-  vpc_id = aws_vpc.ops4.id
+resource "aws_internet_gateway" "ops5_igw" {
+  vpc_id = aws_vpc.ops5.id
 
   tags = {
-    Name = "ops4-igw"
+    Name = "ops5-igw"
   }
 }
 
 # Create route table
-resource "aws_route_table" "ops4_public_rt" {
-  vpc_id = aws_vpc.ops4.id
+resource "aws_route_table" "ops5_public_rt" {
+  vpc_id = aws_vpc.ops5.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ops4_igw.id
+    gateway_id = aws_internet_gateway.ops5_igw.id
   }
 
   tags = {
-    Name = "ops4-public-rt"
+    Name = "ops5-public-rt"
   }
 }
 
 # Create route table association
-resource "aws_route_table_association" "ops4_public_rta" {
-  subnet_id      = aws_subnet.ops4_public.id
-  route_table_id = aws_route_table.ops4_public_rt.id
+resource "aws_route_table_association" "ops5_public_rta" {
+  subnet_id      = aws_subnet.ops5_public.id
+  route_table_id = aws_route_table.ops5_public_rt.id
 }
 
 # Security Group rule: SSH for admin access and TCP 25565 for Minecraft clients
-resource "aws_security_group" "ops4_minecraft_sg" {
-  name        = "op3-minecraft-sg"
+resource "aws_security_group" "ops5_minecraft_sg" {
+  name        = "ops5-minecraft-sg"
   description = "SSH for admin access and TCP 25565 for Minecraft clients"
-  vpc_id      = aws_vpc.ops4.id
+  vpc_id      = aws_vpc.ops5.id
 
   ingress {
     description = "SSH"
@@ -95,22 +95,22 @@ resource "aws_security_group" "ops4_minecraft_sg" {
   }
 
   tags = {
-    Name = "ops4-minecraft-sg"
+    Name = "ops5-minecraft-sg"
   }
 }
 
 
 # Create node
-resource "aws_instance" "ops4_minecraft_node" {
+resource "aws_instance" "ops5_minecraft_node" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.ops4_minecraft_sg.id]
+  vpc_security_group_ids = [aws_security_group.ops5_minecraft_sg.id]
   iam_instance_profile   = "LabInstanceProfile"
-  subnet_id = aws_subnet.ops4_public.id
+  subnet_id = aws_subnet.ops5_public.id
 
   tags = {
-    Name = "ops4-minecraft-node"
+    Name = "ops5-minecraft-node"
   }
 }
 
@@ -124,7 +124,7 @@ locals {
 
 resource "null_resource" "ansible_bridge" {
   # Ensure this only runs AFTER the instance is up
-  depends_on = [aws_instance.ops4_minecraft_node]
+  depends_on = [aws_instance.ops5_minecraft_node]
   # Rerun if playbook has been changed
   triggers = {
     playbook_hash = filesha256("playbook.yml")
@@ -138,14 +138,14 @@ resource "null_resource" "ansible_bridge" {
       type        = "ssh"
       user        = "ubuntu"
       private_key = file(var.ssh_key_path)
-      host        = aws_instance.ops4_minecraft_node.public_ip
+      host        = aws_instance.ops5_minecraft_node.public_ip
     }
   }
 
   # Now that SSH is confirmed, run Ansible locally
   provisioner "local-exec" {
     command = <<EOT
-      ansible-playbook -i '${aws_instance.ops4_minecraft_node.public_ip},' \
+      ansible-playbook -i '${aws_instance.ops5_minecraft_node.public_ip},' \
       --private-key ${var.ssh_key_path} \
       --extra-vars "aws_account_id=${local.account_id} ecr_url=${var.ecr_url} s3_bucket_name=${var.s3_bucket} ecr_repo_name=${var.repo_name} ecr_image_tag=${var.image_tag}" \
       playbook.yml
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "minecraft_node_public_ip" {
   description = "Public IP of the Minecraft node"
-  value       = aws_instance.ops4_minecraft_node.public_ip
+  value       = aws_instance.ops5_minecraft_node.public_ip
 }
 
 output "minecraft_node_private_ip" {
   description = "Private IP of the Minecraft node, used in the Ansible inventory"
-  value       = aws_instance.ops4_minecraft_node.private_ip
+  value       = aws_instance.ops5_minecraft_node.private_ip
 }
 
 output "vpc_id" {
   description = "ID of the provisioned VPC"
-  value       = aws_vpc.ops4.id
+  value       = aws_vpc.ops5.id
 }
